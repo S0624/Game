251c10e69933a99d9f1fe61b48e0a170ee3778a3
大野 栞穂 <fko2247078@stu.o-hara.ac.jp>
2023-04-10 16:04:08 +0900

    テスト

--- Game/main.cpp
+++ Game/main.cpp
@@ -20,12 +20,19 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         return -1;            // エラーが起きたら直ちに終了
     }
 
+    //int witchHandle = LoadGraph("../Data/Witch.png");
+    int witchHandle = LoadGraph("../Data/Witch2.png");
+    int Handle1 = LoadGraph("../Data/charge1.png");
     int handle = LoadGraph("../Data/Knight.png");
 
     // ダブルバッファモード
     SetDrawScreen(DX_SCREEN_BACK);
 
     int animeNum = 0;
+    int animeNum1 = 0;
+    int animeNum2 = 0;
+    int animeFrame = 0;
+    int posX = 200;
 
     while (ProcessMessage() == 0)
     {
@@ -33,18 +40,51 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
         // 画面のクリア
         ClearDrawScreen();
-        animeNum++;
-        if (animeNum > 8)
+        animeFrame++;
+        if (animeFrame > 10)
+        {
+            animeNum++;
+            animeNum1++;
+            animeNum2++;
+            posX += 30;
+            animeFrame = 0;
+        }
+        if (animeNum2 >= 7)
+        {
+            animeNum2 = 0;
+        }
+        if (animeNum >= 9)
         {
             animeNum = 0;
         }
+        if (animeNum1 >= 5)
+        {
+            animeNum1 = 0;
+        }
+        if (posX > Game::kScreenWindth)
+        {
+            posX = 0;
+            animeNum = 0;
+        }
 
+        DrawRectRotaGraph(posX,
+            600,			//表示座標
+            16 * 0, 16 * animeNum2,			//切り取り左上
+            16, 16,							//幅、高さ
+            5.0f, 0.0f,						//拡大率、回転角度
+            handle, true);       
         DrawRectRotaGraph(200,
             200,			//表示座標
-            16 * 0, 16 * animeNum,			//切り取り左上
-            16, 16,							//幅、高さ
+            108 * 0, 48 * animeNum1,			//切り取り左上
+            108, 48,							//幅、高さ
             3.0f, 0.0f,						//拡大率、回転角度
-            handle, true);
+            Handle1, true);
+        DrawRectRotaGraph(200,
+            500,			//表示座標
+            108 * 0, 48 * animeNum,			//切り取り左上
+            108, 48,							//幅、高さ
+            3.0f, 0.0f,						//拡大率、回転角度
+            witchHandle, true);
 
         // 裏画面を表画面を入れ替える
         ScreenFlip();
