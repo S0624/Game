9e378426935def311b2fd6559f2b73a50fe688ee
S0624 <mmasyumaro.012345@gmail.com>
2023-04-11 18:46:03 +0900

    まじょのいらすとのきりはり

--- Game/main.cpp
+++ Game/main.cpp
@@ -20,10 +20,13 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         return -1;            // エラーが起きたら直ちに終了
     }
 
+    SetAlwaysRunFlag(true);
     //int witchHandle = LoadGraph("../Data/Witch.png");
-    int witchHandle = LoadGraph("../Data/Witch2.png");
-    int Handle1 = LoadGraph("../Data/charge1.png");
-    int handle = LoadGraph("../Data/Knight.png");
+    int witchAttackHandle = LoadGraph("../Data/Witch2.png");
+    int witchChargeHandle = LoadGraph("../Data/charge1.png");
+    int NiwatoriRunHandle = LoadGraph("../Data/Niwatori.png");
+    int nekochanAttackHandle = LoadGraph("../Data/nekochanmachine.png");
+    int nekochanHandle = LoadGraph("../Data/Knight.png");
 
     // ダブルバッファモード
     SetDrawScreen(DX_SCREEN_BACK);
@@ -31,6 +34,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     int animeNum = 0;
     int animeNum1 = 0;
     int animeNum2 = 0;
+    int animeNum3 = 0;
+    int animeNum4 = 0;
     int animeFrame = 0;
     int posX = 200;
 
@@ -46,8 +51,14 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
             animeNum++;
             animeNum1++;
             animeNum2++;
+            animeNum3++;
+            animeNum4++;
             posX += 30;
             animeFrame = 0;
+        }
+        if (animeNum3 >= 10)
+        {
+            animeNum3 = 0;
         }
         if (animeNum2 >= 7)
         {
@@ -61,6 +72,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         {
             animeNum1 = 0;
         }
+        if (animeNum4 >= 4)
+        {
+            animeNum4 = 0;
+        }
         if (posX > Game::kScreenWindth)
         {
             posX = 0;
@@ -71,20 +86,32 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
             600,			//表示座標
             16 * 0, 16 * animeNum2,			//切り取り左上
             16, 16,							//幅、高さ
-            5.0f, 0.0f,						//拡大率、回転角度
-            handle, true);       
+            3.0f, 0.0f,						//拡大率、回転角度
+            nekochanHandle, true);
+        DrawRectRotaGraph(posX,
+            100,			//表示座標
+            32 * 0, 32 * animeNum3,			//切り取り左上
+            32, 32,							//幅、高さ
+            3.0f, 0.0f,						//拡大率、回転角度
+            NiwatoriRunHandle, true);
+        DrawRectRotaGraph(100,
+            400,			//表示座標
+            48 * 0, 32 * animeNum4,			//切り取り左上
+            48, 32,							//幅、高さ
+            3.0f, 0.0f,						//拡大率、回転角度
+            nekochanAttackHandle, true);       
         DrawRectRotaGraph(200,
             200,			//表示座標
             108 * 0, 48 * animeNum1,			//切り取り左上
             108, 48,							//幅、高さ
             3.0f, 0.0f,						//拡大率、回転角度
-            Handle1, true);
+            witchChargeHandle, true);
         DrawRectRotaGraph(200,
             500,			//表示座標
             108 * 0, 48 * animeNum,			//切り取り左上
             108, 48,							//幅、高さ
             3.0f, 0.0f,						//拡大率、回転角度
-            witchHandle, true);
+            witchAttackHandle, true);
 
         // 裏画面を表画面を入れ替える
         ScreenFlip();
