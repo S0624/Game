cff6b1e7874ea8719fd9d8575319f6ded588ea48
S0624 <mmasyumaro.012345@gmail.com>
2023-04-14 18:49:32 +0900

    wa

--- Game/main.cpp
+++ Game/main.cpp
@@ -1,5 +1,5 @@
 #include "DxLib.h"
-
+#include"Pad.h"
 #include "math.h"
 #include "game.h"
 
@@ -21,9 +21,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     }
 
     SetAlwaysRunFlag(true);
-    //int witchHandle = LoadGraph("../Data/Witch.png");
-    int witchAttackHandle = LoadGraph("../Data/Witch2.png");
-    int witchChargeHandle = LoadGraph("../Data/charge1.png");
+    int witchHandle = LoadGraph("../Data/Witch.png");
+    //int witchAttackHandle = LoadGraph("../Data/Witch2.png");
+    //int witchChargeHandle = LoadGraph("../Data/charge1.png");
     int NiwatoriRunHandle = LoadGraph("../Data/Niwatori.png");
     int nekochanAttackHandle = LoadGraph("../Data/nekochanmachine.png");
     int nekochanHandle = LoadGraph("../Data/Knight.png");
@@ -32,13 +32,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     SetDrawScreen(DX_SCREEN_BACK);
 
     int animeNum = 0;
-    int animeNum1 = 0;
-    int animeNum2 = 0;
-    int animeNum3 = 0;
-    int animeNum4 = 0;
     int animeFrame = 0;
-    int posX = 200;
 
+    int animeMax = 8;
+    int animeIndex = 0;
     while (ProcessMessage() == 0)
     {
         LONGLONG time = GetNowHiPerformanceCount();
@@ -46,72 +43,35 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         // 画面のクリア
         ClearDrawScreen();
         animeFrame++;
+        Pad::update();
         if (animeFrame > 10)
         {
             animeNum++;
-            animeNum1++;
-            animeNum2++;
-            animeNum3++;
-            animeNum4++;
-            posX += 30;
             animeFrame = 0;
         }
-        if (animeNum3 >= 10)
+        if (animeNum >= animeMax)
         {
-            animeNum3 = 0;
-        }
-        if (animeNum2 >= 7)
-        {
-            animeNum2 = 0;
-        }
-        if (animeNum >= 9)
-        {
-            animeNum = 0;
-        }
-        if (animeNum1 >= 5)
-        {
-            animeNum1 = 0;
-        }
-        if (animeNum4 >= 4)
-        {
-            animeNum4 = 0;
-        }
-        if (posX > Game::kScreenWindth)
-        {
-            posX = 0;
             animeNum = 0;
         }
 
-        DrawRectRotaGraph(posX,
-            600,			//表示座標
-            16 * 0, 16 * animeNum2,			//切り取り左上
-            16, 16,							//幅、高さ
-            3.0f, 0.0f,						//拡大率、回転角度
-            nekochanHandle, true);
-        DrawRectRotaGraph(posX,
-            100,			//表示座標
-            32 * 0, 32 * animeNum3,			//切り取り左上
-            32, 32,							//幅、高さ
-            3.0f, 0.0f,						//拡大率、回転角度
-            NiwatoriRunHandle, true);
-        DrawRectRotaGraph(100,
-            400,			//表示座標
-            48 * 0, 32 * animeNum4,			//切り取り左上
-            48, 32,							//幅、高さ
-            3.0f, 0.0f,						//拡大率、回転角度
-            nekochanAttackHandle, true);       
-        DrawRectRotaGraph(200,
-            200,			//表示座標
-            108 * 0, 48 * animeNum1,			//切り取り左上
-            108, 48,							//幅、高さ
-            3.0f, 0.0f,						//拡大率、回転角度
-            witchChargeHandle, true);
+        //int pad = GetJoypadInputState(DX_INPUT_KEY_PAD1);
+        //if (GetJoypadInputState(DX_INPUT_KEY_PAD1))
+        //if(Pad::isPress(DX_INPUT_KEY_PAD1))
+        if(Pad::isPress(DX_INPUT_PAD1))
+        {
+            animeIndex += 1;
+            if (animeIndex > 10)
+            {
+                animeIndex = 0;
+            }
+        }
+
         DrawRectRotaGraph(200,
             500,			//表示座標
-            108 * 0, 48 * animeNum,			//切り取り左上
-            108, 48,							//幅、高さ
+            48 * animeIndex, 48 * animeNum,			//切り取り左上
+            48 * 1, 48,							//幅、高さ
             3.0f, 0.0f,						//拡大率、回転角度
-            witchAttackHandle, true);
+            witchHandle, true);
 
         // 裏画面を表画面を入れ替える
         ScreenFlip();
